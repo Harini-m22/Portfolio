c80271fd3de9560e434a1070332495a013e9a98d
Harini-m22 <harinisusi2@gmail.com>
2025-04-16 23:54:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function downloadResume() {
+    const link = document.createElement("a");
+    link.href = "resume.pdf"; // Make sure 'resume.pdf' exists in the same folder
+    link.download = "resume.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  
